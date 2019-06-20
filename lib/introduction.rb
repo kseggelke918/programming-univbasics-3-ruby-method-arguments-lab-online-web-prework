6238ddf11ee706fd78fd2e6d945768cb7dc5f736
Katie Seggelke <katie.seggelke@gmail.com>
2019-06-20 21:04:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,12 +3,11 @@ def introduction
   puts "Hi, my name is #{name}"
 end
 
-introduction
-
 def introduction_with_language
   name = "Dan"
   language = "React"
-  puts "Hi, my name is #{name} and I am learning to program in #{language}.
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
+introduction
 introduction_with_language
