01684632b1fa55c25533cbfd31757cb59e6c0bfb
Katie Seggelke <katie.seggelke@gmail.com>
2019-06-20 21:00:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,13 +1,13 @@
+name = "Josh"
+launguage = "Ruby"
+
 def introduction
-  name = "Josh"
   puts "Hi, my name is #{name}"
 end
 
 introduction
 
 def introduction_with_language
-  name = "Josh"
-  launguage = "Ruby"
   puts "Hi, my name is #{name} and I am learning to program in #{launguage}"
 end 
 
